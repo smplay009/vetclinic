dd02a6a316f140ab3b1aea2266c5b56fdfdb4b8e
cookie <d.p0n02i@gmail.com>
2025-11-04 02:34:59 +0600

    add gunicorn for render deploy

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vetclinic1.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
